0885bee30ea04ed7831050c58ccdc44a9cca0e28
AyodeleEbenezer <oluwabusola.ayodele@gmail.com>
2018-12-06 13:20:57 +0100

    add btn

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,14 +8,9 @@ import { HeroesComponent } from './heroes/heroes.component';
 import { HeroDetailComponent } from './hero-detail/hero-detail.component';
 import { MessagesComponent } from './messages/messages.component';
 import { DashboardComponent } from './dashboard/dashboard.component';
-// import { HttpClientModule } from '@angular/common/http';
-// import { HttpClientInMemoryWebApiModule } from 'angular-in-memory-web-api';
-
-
-// HttpClientModule,
-// HttpClientInMemoryWebApiModule.forRoot(
-//   InMemoryDataService, { dataEncapsulation: false }
-// )
+import { HttpClientModule } from '@angular/common/http';
+import { HttpClientInMemoryWebApiModule } from 'angular-in-memory-web-api';
+import { InMemoryDataService }  from './in-memory-data.service';
 
 @NgModule({
   declarations: [
@@ -29,8 +24,10 @@ import { DashboardComponent } from './dashboard/dashboard.component';
     BrowserModule,
     AppRoutingModule,
     FormsModule,
-    // HttpClientModule,
-    // HttpClientInMemoryWebApiModule,
+    HttpClientModule,
+    HttpClientInMemoryWebApiModule.forRoot(
+      InMemoryDataService, { dataEncapsulation: false }
+    )
   ],
   providers: [],
   bootstrap: [AppComponent]
